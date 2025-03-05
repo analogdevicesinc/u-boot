d1fcb38958dfd67128c2efb039cd915a338933ba
Eduardo Grande <eduardo.grande@analog.com>
2025-03-05 10:59:29 +0100

    i2c: adrv906x: add I2C support and secondary memory map
    Add ADI I2C controller driver support and configure secondary
    memory map for expanded peripheral access.
    
    Signed-off-by: Eduardo Grande <eduardo.grande@analog.com>

--- arch/arm/mach-adrv906x/soc.c
+++ arch/arm/mach-adrv906x/soc.c
@@ -55,6 +55,14 @@ static struct mm_region adrv906x_mem_map[] = {
 		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
 			 PTE_BLOCK_NON_SHARE |
 			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	},
+	{
+		.virt = 0x24000000UL,
+		.phys = 0x24000000UL,
+		.size = 0x04000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
 	},{
 		/* List terminator */
 		0,
