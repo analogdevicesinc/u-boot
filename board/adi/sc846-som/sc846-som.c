7a1a13efa356dceb4e2b5c294502476417d85336
UtsavAgarwalADI <utsav.agarwal@analog.com>
2026-02-02 18:51:43 +0100

    board: adi: Add SC846 SoM board support
    Co-authored-by: Ozan Durgut <ozan.durgut@analog.com>
    Signed-off-by: UtsavAgarwalADI <utsav.agarwal@analog.com>
    Signed-off-by: Ozan Durgut <ozan.durgut@analog.com>

---
+++ board/adi/sc846-som/sc846-som.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * (C) Copyright 2026 - Analog Devices, Inc.
+ */
+
+#include <config.h>
+#include <phy.h>
+#include <asm/u-boot.h>
+#include <asm/arch-adi/sc5xx/sc5xx.h>
+#include <asm/arch-adi/sc5xx/soc.h>
+#include <asm/armv8/mmu.h>
+#include "../carriers/somcrr.h"
+
+int board_init(void)
+{
+	return 0;
+}
