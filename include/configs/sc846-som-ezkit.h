c79db1d29e3fc9cc69235a99e4c4ab8b40a3e40c
Arturs Artamonovs <Arturs.Artamonovs@analog.com>
2026-05-04 11:48:33 +0100

    board: adi: Add SC846-SOM-EZKIT carrier board support
    Signed-off-by: Arturs Artamonovs <Arturs.Artamonovs@analog.com>

---
+++ include/configs/sc846-som-ezkit.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * (C) Copyright 2026 - Analog Devices, Inc.
+ */
+
+#ifndef __CONFIG_SC846_SOM_EZKIT_H
+#define __CONFIG_SC846_SOM_EZKIT_H
+
+#include "sc846-som.h"
+
+#endif
