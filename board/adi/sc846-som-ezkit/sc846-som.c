c5490b9d58efb0e1d6b8a4b21e0376fec38f1b20
Arturs Artamonovs <arturs.artamonovs@analog.com>
2026-05-05 12:17:13 +0100

    arm: dts: sc846: Enable EMAC0 Ethernet on EZKIT carrier board
    Signed-off-by: Arturs Artamonovs <arturs.artamonovs@analog.com>

--- board/adi/sc846-som-ezkit/sc846-som.c
+++ board/adi/sc846-som-ezkit/sc846-som.c
@@ -6,17 +6,15 @@
 #include <config.h>
 #include <phy.h>
 #include <asm/u-boot.h>
-#include <asm/arch-adi/sc5xx/sc5xx.h>
-#include <asm/arch-adi/sc5xx/soc.h>
+#include <asm/arch/sc5xx.h>
+#include <asm/arch/soc.h>
 #include <asm/armv8/mmu.h>
 #include "../carriers/somcrr.h"
 
 int board_init(void)
 {
-
-	if (IS_ENABLED(CONFIG_ADI_CARRIER_SOMCRR2_EZKIT)) {
+	if (IS_ENABLED(CONFIG_ADI_CARRIER_SOMCRR2_EZKIT))
 		adi_somcrr_init_ethernet();
-	}
 
 	sc5xx_enable_rgmii();
 
