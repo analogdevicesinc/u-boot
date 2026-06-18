0605b8e2b2f18bfa9c24ecfc5a98053bf508ec25
Caleb Ethridge <caleb.ethridge@analog.com>
2026-06-18 12:53:11 -0400

    arm: mach-sc5xx: Implement Falcon mode boot support in SPL
    Falcon boot implementation for the SC5xx family of SoCs for
    booting from the SPI flash and the eMMC for boards that support it.
    
    The SPL will boot directly into the kernel unless the boot mode
    selector is set to 0, the Debug/Host/JTAG boot mode.
    
    Signed-off-by: Caleb Ethridge <caleb.ethridge@analog.com>

--- arch/arm/mach-sc5xx/spl.c
+++ arch/arm/mach-sc5xx/spl.c
@@ -9,6 +9,7 @@
 #include <spl.h>
 #include <asm/arch/sc5xx.h>
 #include <asm/arch/spl.h>
+#include <linux/libfdt.h>
 #include "init/clkinit.h"
 #include "init/dmcinit.h"
 
@@ -22,7 +23,12 @@ u32 bmode;
 
 int spl_start_uboot(void)
 {
-	return adi_start_uboot_proper;
+	sc5xx_get_boot_mode(&bmode);
+
+	if (bmode == 0)
+		return 1;
+
+	return 0;
 }
 
 unsigned int spl_spi_get_default_speed(void)
@@ -43,19 +49,34 @@ unsigned int spl_spi_get_default_cs(void)
 void board_boot_order(u32 *spl_boot_list)
 {
 	const char *bmodestring = sc5xx_get_boot_mode(&bmode);
-
 	printf("ADI Boot Mode: 0x%x (%s)\n", bmode, bmodestring);
 
-	/*
-	 * By default everything goes back to the bootrom, where we'll read table
-	 * parameters and ask for another image to be loaded
-	 */
-	spl_boot_list[0] = BOOT_DEVICE_BOOTROM;
+	if (bmode != 0 && spl_start_uboot()) {
+		spl_boot_list[0] = BOOT_DEVICE_BOOTROM;
+		return;
+	}
 
-	if (bmode == 0) {
+	switch (bmode) {
+	case 0:
 		printf("SPL execution has completed.  Please load U-Boot Proper via JTAG");
 		while (1)
 			;
+	case 1:
+		adi_sf_default_bus = 2;
+		adi_sf_default_cs = 1;
+		spl_boot_list[0] = BOOT_DEVICE_SPI;
+		break;
+	case 5:
+		adi_sf_default_bus = 0;
+		adi_sf_default_cs = 0;
+		spl_boot_list[0] = BOOT_DEVICE_SPI;
+		break;
+	case 6:
+		spl_boot_list[0] = BOOT_DEVICE_MMC1;
+		break;
+	default:
+		spl_boot_list[0] = BOOT_DEVICE_BOOTROM;
+		break;
 	}
 }
 
@@ -98,3 +119,66 @@ void board_init_f(ulong dummy)
 	preloader_console_init();
 }
 
+void spl_board_prepare_for_linux(void)
+{
+	void *fdt = (void *)CONFIG_SPL_PAYLOAD_ARGS_ADDR;
+	int nodeoff;
+	int ret;
+	char bootargs[512];
+	char adi_bootargs[256];
+	const char *mode_bootargs = NULL;
+
+	switch (bmode) {
+	case 1:
+		mode_bootargs = "rootfstype=ubifs root=ubi0:rootfs ubi.mtd=3 rw";
+		break;
+	case 5:
+		mode_bootargs = "rootfstype=ubifs root=ubi0:rootfs ubi.mtd=3 rw";
+		break;
+	case 6:
+		/* eMMC: matches the board env "mmcargs" */
+		mode_bootargs = "root=/dev/mmcblk0p2 rw rootfstype=ext4 rootwait";
+		break;
+	default:
+		break;
+	}
+
+	if (!mode_bootargs)
+		return;
+
+	ret = snprintf(adi_bootargs, sizeof(adi_bootargs),
+#if defined(CONFIG_SC59X_64)
+		       "earlycon=adi_uart,0x31003000 "
+		       "console=ttySC0,%d vmalloc=512M",
+		       CONFIG_BAUDRATE);
+#else
+		       "earlyprintk=serial,uart0,%d "
+		       "console=ttySC0,%d vmalloc=512M",
+		       CONFIG_BAUDRATE, CONFIG_BAUDRATE);
+#endif
+	if (ret <= 0 || ret >= sizeof(adi_bootargs))
+		return;
+
+	ret = snprintf(bootargs, sizeof(bootargs), "%s %s",
+		       mode_bootargs, adi_bootargs);
+	if (ret <= 0 || ret >= sizeof(bootargs))
+		return;
+
+	ret = fdt_check_header(fdt);
+	if (ret)
+		return;
+
+	nodeoff = fdt_path_offset(fdt, "/chosen");
+	if (nodeoff < 0)
+		nodeoff = fdt_add_subnode(fdt, 0, "chosen");
+	if (nodeoff < 0)
+		return;
+
+	fdt_setprop_string(fdt, nodeoff, "bootargs", bootargs);
+	printf("SC5xx SPL bootargs: %s\n", bootargs);
+}
+
+void *board_spl_fit_buffer_addr(ulong fit_size, int sectors, int bl_len)
+{
+	return (void *)CONFIG_SPL_LOAD_FIT_ADDRESS;
+}
