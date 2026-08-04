analogdevicesinc/u-boot

commit 3285a564d2b741fdcc6161de39f14c76d7229e06

arch: sc5xx: Add SC846 DDR init hooks
Signed-off-by: Philip Molloy <philip@philipmolloy.com>

arch/arm/mach-sc5xx/init/clkinit.c
@@ -10,7 +10,14 @@
 #include <asm/io.h>
 #include <linux/types.h>
 #include "clkinit.h"
+
+#ifdef CONFIG_SC5XX_DMC_INIT
 #include "dmcinit.h"
+#endif
+
+#ifdef CONFIG_SC846
+#include "EHP2_LP4_1D_2000/EHP2_LPDDR4_1D_2000_Core1.h"
+#endif
 
 #ifdef CONFIG_CGU0_SCLK0_DIV
 	#define VAL_CGU0_SCLK0_DIV CONFIG_CGU0_SCLK0_DIV
@@ -225,10 +232,10 @@ struct CGU_Settings {
 	(defined(CONFIG_SC57X) || defined(CONFIG_SC58X))
 	#define ARMCLK_IN	0
 	#define ARMCLK_RATIO	SYSCLK_n_RATIO(0)
-#elif (5 == CONFIG_CDU0_CLKO2) && defined(CONFIG_SC59X_64)
+#elif (5 == CONFIG_CDU0_CLKO2) && defined(CONFIG_ARM64)
 	#define ARMCLK_IN	0
 	#define ARMCLK_RATIO	CCLK2_n_RATIO(0)
-#elif (7 == CONFIG_CDU0_CLKO2) && defined(CONFIG_SC59X_64)
+#elif (7 == CONFIG_CDU0_CLKO2) && defined(CONFIG_ARM64)
 	#define ARMCLK_IN	CDU0_CGU1_CLKIN
 	#define ARMCLK_RATIO	CCLK2_n_RATIO(1)
 #endif
@@ -456,7 +463,7 @@ static void CGU_Init(const struct CGU_Settings *pCGU)
 {
 	const uintptr_t b = pCGU->rbase;
 
-#if defined(CONFIG_SC59X) || defined(CONFIG_SC59X_64)
+#if defined(CONFIG_SC59X) || defined(CONFIG_SC59X_64) || defined(CONFIG_SC846)
 	if (readl(b + CGU_STAT) & BITM_CGU_STAT_PLLEN)
 		writel(BITM_CGU_PLLCTL_PLLEN, b + CGU_PLLCTL);
 
@@ -467,14 +474,14 @@ static void CGU_Init(const struct CGU_Settings *pCGU)
 	if (readl(b + CGU_STAT) & BITM_CGU_STAT_PLLBP)
 		Active_To_Fullon(pCGU);
 
-#if defined(CONFIG_SC59X) || defined(CONFIG_SC59X_64)
+#if defined(CONFIG_SC59X) || defined(CONFIG_SC59X_64) || defined(CONFIG_SC846)
 	dmcdelay(1000);
 #endif
 
 	program_cgu(pCGU);
 }
 
-void cgu_init(void)
+__attribute__((optimize("O0"))) void cgu_init(void)
 {
 	const struct CGU_Settings dividers0 = {
 		.rbase =		CGU0_REGBASE,
@@ -529,28 +536,38 @@ void cdu_init(void)
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO7, REG_CDU0_CFG7, 7);
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO8, REG_CDU0_CFG8, 8);
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO9, REG_CDU0_CFG9, 9);
-#ifdef CONFIG_CDU0_CLKO10
+#if (IS_ENABLED(CONFIG_CDU0_CLKO10))
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO10, REG_CDU0_CFG10, 10);
 #endif
-#ifdef CONFIG_CDU0_CLKO12
+#if (IS_ENABLED(CONFIG_CDU0_CLKO12))
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO12, REG_CDU0_CFG12, 12);
 #endif
-#ifdef CONFIG_CDU0_CLKO13
+#if (IS_ENABLED(CONFIG_CDU0_CLKO13))
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO13, REG_CDU0_CFG13, 13);
 #endif
-#ifdef CONFIG_CDU0_CLKO14
+#if (IS_ENABLED(CONFIG_CDU0_CLKO14))
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO14, REG_CDU0_CFG14, 14);
 #endif
 }
 
 void clks_init(void)
 {
+#if (IS_ENABLED(CONFIG_SC5XX_DMC_INIT))
 	adi_dmc_reset_lanes(true);
+#endif
 
 	cdu_init();
 	cgu_init();
 
 	adi_config_third_pll();
 
+#if (IS_ENABLED(CONFIG_SC5XX_DMC_INIT))
 	adi_dmc_reset_lanes(false);
+#endif
+
+#if (IS_ENABLED(CONFIG_SC846))
+	pre_reset_init_lpddr4();
+	NOP();
+	post_reset_init_lpddr4();
+#endif
 }

arch/arm/mach-sc5xx/Kconfig
@@ -26,17 +26,20 @@ config SC57X
 	select COMMON_CLK_ADI_SC57X
 	select CPU_V7A
 	select TARGET_SC573_EZLITE
+	select SC5XX_DMC_INIT
 
 config SC58X
 	bool "SC58x series"
 	select COMMON_CLK_ADI_SC58X
 	select CPU_V7A
+	select SC5XX_DMC_INIT
 
 config SC59X
 	bool "SC59x 32-bit series"
 	select COMMON_CLK_ADI_SC594
 	select CPU_V7A
 	select NOP_PHY if PHY
+	select SC5XX_DMC_INIT
 
 config SC59X_64
 	bool "SC59x 64-bit series"
@@ -46,7 +49,17 @@ config SC59X_64
 	select GICV3_SUPPORT_GIC600
 	select MMC_SDHCI_ADMA_FORCE_32BIT
 	select NOP_PHY if PHY
+	select SC5XX_DMC_INIT
 
+config SC846
+	bool "SC846 64-bit series"
+	select ARM64
+	select COMMON_CLK_ADI_SC846
+	select GICV3
+	select GICV3_SUPPORT_GIC600
+	select GIC_600_CLEAR_RDPD
+	select MMC_SDHCI_ADMA_FORCE_32BIT
+	select NOP_PHY if PHY
 endchoice
 
 if SC57X
@@ -115,6 +128,81 @@ config TARGET_SC598_SOM_EZKIT
 endchoice
 
 endif
+
+if SC846
+
+choice
+	prompt "SC846 64-bit board select"
+
+config TARGET_SC846_SOM
+	bool
+	prompt "SC846-SOM (no carrier board)"
+
+endchoice
+endif
+
+config SC5XX_UBOOT_SPL_OFFSET
+	hex "SPL offset"
+	default 0x0
+	help
+	  The default offset where the SPL is located.
+
+config SC5XX_UBOOT_OFFSET
+	hex "U-Boot offset"
+	default 0x40000
+	help
+	  The default offset where u-boot is located.
+
+config SC5XX_FITIMAGE_OFFSET
+	hex "FitImage offset"
+	default 0x1a0000
+	help
+	  The default offset where the fitImage is located.
+
+config SC5XX_ROOTFS_OFFSET
+	hex "RootFS offset"
+	default 0x102000
+	help
+	  The default offset where the rootfs is located.
+
+config SC5XX_LOADADDR
+	hex "Load address"
+	default 0x90000000
+	help
+	  The default load address for u-boot.
+
+menu "Binman configuration"
+config SC5XX_USE_BINMAN
+	bool "Use binman for final image"
+	select BINMAN
+	help
+	  If you wish to use binman to assemble an image, say 'Y' here.
+	  This will enable binman-specific sections in the device tree.
+
+config SC5XX_BINMAN_FILENAME
+	string "Image name"
+	default "sc5xx-image.img"
+	depends on SC5XX_USE_BINMAN
+	help
+	  The name of the image that will be created by binman.
+	  This is used to create the final image.
+
+config SC5XX_FITIMAGE_NAME
+	string "FitImage name"
+	default "fitImage"
+	depends on SC5XX_USE_BINMAN
+	help
+	  The name of the fitImage to be packed by binman.
+	  This is used to create the final image.
+
+config SC5XX_ROOTFS_NAME
+	string "RootFS name"
+	default "rootfs"
+	depends on SC5XX_USE_BINMAN
+	help
+	  The name of the rootfs to be packed by binman.
+	  This is used to create the final image.
+endmenu
 
 config ADI_IMAGE
 	string "ADI fitImage type"
@@ -269,6 +357,14 @@ config ADI_USE_DDR2
 	  the SoC that you are using to determine if DDR2 mode is supported.
 	  This also requires that DDR2 memory is present on the board or it
 	  will probably cause strange failure.
+
+config SC5XX_DMC_INIT
+	bool "Initialize the DMC"
+	default y
+	help
+	  Initialize the DMC during hardware initialization. This is
+	  required if you are using external DDR memory and is limited to
+	  ADSP-SC5XX architecture
 
 menu "Clock configuration"
 
@@ -563,6 +659,7 @@ config PINCTRL_ADI
 	bool
 	default y
 
+source "board/adi/sc846-som/Kconfig"
 source "board/adi/sc598-som-ezkit/Kconfig"
 source "board/adi/sc598-som-ezlite/Kconfig"
 source "board/adi/sc594-som-ezkit/Kconfig"

arch/arm/mach-sc5xx/init/Makefile
@@ -5,4 +5,7 @@
 # Written by Timesys Corporation
 #
 
-obj-y += dmcinit.o clkinit.o
+obj-y = clkinit.o
+
+obj-$(CONFIG_SC846) += EHP2_LP4_1D_2000/
+obj-$(CONFIG_SC5XX_DMC_INIT) += dmcinit.o

arch/arm/mach-sc5xx/spl.c
@@ -10,7 +10,10 @@
 #include <asm/arch/sc5xx.h>
 #include <asm/arch/spl.h>
 #include "init/clkinit.h"
+
+#if (IS_ENABLED(CONFIG_SC5XX_DMC_INIT))
 #include "init/dmcinit.h"
+#endif
 
 static bool adi_start_uboot_proper;
 
@@ -67,7 +70,7 @@ int32_t __weak adi_rom_boot_hook(struct ADI_ROM_BOOT_CONFIG *config, int32_t cau
 int board_return_to_bootrom(struct spl_image_info *spl_image,
 			    struct spl_boot_device *bootdev)
 {
-#if CONFIG_ADI_SPL_FORCE_BMODE != 0
+#if !(IS_ENABLED(CONFIG_ADI_SPL_FORCE_BMODE))
 	// see above
 	if (bmode != 0 && bmode != 3)
 		bmode = CONFIG_ADI_SPL_FORCE_BMODE;
@@ -88,7 +91,9 @@ void board_init_f(ulong dummy)
 	int ret;
 
 	clks_init();
+#if (IS_ENABLED(CONFIG_SC5XX_DMC_INIT))
 	DMC_Config();
+#endif
 	sc5xx_soc_init();
 
 	ret = spl_early_init();

commit be8d39c88b6e19ab7e58fd6db2239287fdabb91a

arch: sc5xx: Revert CDU0 CLKO10/12/13/14 IS_ENABLED guards
CONFIG_CDU0_CLKOxx are "int" Kconfig options carrying a clock-source
select value (range 1..7). IS_ENABLED() only evaluates
true when a symbol is defined to 1, so any output configured with a
value other than 1 was silently skipped.

This, for example, left the clocks for the eMMC on the sc598
unconfigured, preventing the mmc from working in U-boot.

Restore the original "#ifdef" guards so the outputs are configured
whenever the board provides a value.

Fixes: 3285a564d2b ("arch: sc5xx: Add SC846 DDR init hooks")
Signed-off-by: Caleb Ethridge <caleb.ethridge@analog.com>

arch/arm/mach-sc5xx/init/clkinit.c
@@ -536,16 +536,16 @@ void cdu_init(void)
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO7, REG_CDU0_CFG7, 7);
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO8, REG_CDU0_CFG8, 8);
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO9, REG_CDU0_CFG9, 9);
-#if (IS_ENABLED(CONFIG_CDU0_CLKO10))
+#ifdef CONFIG_CDU0_CLKO10
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO10, REG_CDU0_CFG10, 10);
 #endif
-#if (IS_ENABLED(CONFIG_CDU0_CLKO12))
+#ifdef CONFIG_CDU0_CLKO12
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO12, REG_CDU0_CFG12, 12);
 #endif
-#if (IS_ENABLED(CONFIG_CDU0_CLKO13))
+#ifdef CONFIG_CDU0_CLKO13
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO13, REG_CDU0_CFG13, 13);
 #endif
-#if (IS_ENABLED(CONFIG_CDU0_CLKO14))
+#ifdef CONFIG_CDU0_CLKO14
 	CONFIGURE_CDU0(CONFIG_CDU0_CLKO14, REG_CDU0_CFG14, 14);
 #endif
 }